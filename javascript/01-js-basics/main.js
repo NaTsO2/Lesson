b08c973786466e0b7eacc777a0a1d1922ba4cfc4
NaTsO2 <narantsogt.md@gmail.com>
2025-02-19 12:48:21 +0800

    js hicheel

--- javascript/01-js-basics/main.js
+++ javascript/01-js-basics/main.js
@@ -1,20 +1,176 @@
-console.log("Сайн уу? JS!")
+// console.log("Сайн уу? JS!")
 
-// let message = "Hello, World!";
-// console.log(message);
+// // let message = "Hello, World!";
+// // console.log(message);
 
-// let age = 42;
-// console.log(age);
+// // let age = 42;
+// // console.log(age);
 
-// let gpa=4.5;
-// console.log(gpa);
+// // let gpa=4.5;
+// // console.log(gpa);
 
-// let isStudent=true;
-// console.log(isStudent);
+// // let isStudent=true;
+// // console.log(isStudent);
 
-// let x;
-// console.log(x);
+// // let x;
+// // console.log(x);
 
-// let n=null;
-// console.log(n);
+// // let n=null;
+// // console.log(n);
+
+// let a=10;
+// let b=20;
+
+// console.log("a+b=",a+b);
+// console.log("a-b=",a-b);
+// console.log("a*b=",a*b);
+// console.log("a/b=",a/b);
+// console.log("a%b=",a%b);
+
+// console.log("a===b",a===b);
+// console.log("a!==b",a!==b);
+// console.log("a>b",a>b);
+// console.log("a<b",a<b);
+// console.log("a>=b",a>=b);
+// console.log("a<=b",a<=b);
+
+// console.log("a === b && a < b", a === b && a < b);
+// console.log("a !== b && a < b", a !== b && a < b);
+
+// console.log("a === b || a < b", a === b || a < b);
+// console.log("a !== b || a < b", a !== b || a < b);
+
+
+// let birthYears = [1991, 1992, 1993, 1994, 1995, 1996, 1997, 1998, 1999, 2000];
+// let personNames = new Array("Dorj", "Bat", "Naraa", "Saraa", "Nar", "Od", "Sar", "Mundur", "Tsetsegee")
+// // birthYears[9] = 2024;
+
+// console.log("Төрсөн Он:", birthYears);
+// console.log("Нэр:", personNames);
+
+
+// let square = [[1,2,3],[4,5,6],[7,8,9]];
+// console.log(square);
+
+// console.log("0:",birthYears [0]);
+// console.log("Түүний нэр:", personNames[0], "Төрсөн Он:", birthYears [1]);
+// console.log(birthYears [2]);
+// console.log(birthYears [3]);
+// console.log(birthYears [4]);
+// console.log(birthYears [5]);
+// console.log(birthYears [6]);
+// console.log(birthYears [7]);
+// console.log(birthYears [8]);
+// console.log(birthYears [9] = 2024);
+
+// console.log(birthYears.length)
+
+// for(let i = 0; i < 100; i++ ) {
+//     console.log(i);
+// }
+
+// let count = 1;
+// while (count <= 10) {
+//     console.log("while loop:", count);
+//     count = count + 1;
+// };
+
+// let too = 1;
+
+// do {
+//     console.log (too);
+//     too = too + 1;   
+// } while(too <= 10);
+
+// let sum = 0;
+// for (let a = 1; a < 10; a++ ) {
+//     console.log('bodlogo', a);
+//     sum = sum + a;
+// };
+// console.log(sum);
+
+// let niilber = 0;
+// let q = 1;
+
+// while (q < 10) {
+//     niilber = niilber + q;
+//     q++;
+// }
+// console.log("sum =", niilber);
+
+// let nas = 17;
+
+// if (nas >= 18) {
+//     console.log ("Таны нас:", nas);
+//     console.log("Та насанд хүрсэн байна");
+// } else {
+//     console.log("Та насанд хүрээгүй байна");
+// };
+
+// let bmi = 24;
+
+// if (bmi < 18.5) {
+//     console.log("Та тураалтай байна");
+// } else if (bmi > 18.5 && bmi < 24.9) {
+//     console.log("Та хэвийн жинтэй байна");
+// } else if (bmi >= 25 && bmi <= 29.9) {
+//     console.log("Та илүүдэл жинтэй байна");
+// } else if (bmi >= 30 && bmi <=349.9) {
+//     console.log("Та таргалалттай байна");
+// } else (bmi < 35 ) 
+// {
+//     console.log("Та хэт их таргалалттай байна");
+// };
+
+let a = 3;
+let b = 5;
+let c = 7;
+let p=a+b+c;
+
+console.log("1. Гурвалжингийн приметр =",p);
+
+let x = [1,3,5,7,8.7];
+let sum=x[0]+x[1]+x[2]+x[3];
+
+console.log("2. Өгөгдсөн тоонуудын нийлбэр",sum);
+
+
+let h = 10;
+let w = 5;
+let s=h*w;
+let pr=(h+w)*2;
+console.log("3.тэгш өнцөгтийн талбай=",s,"тэгш өнцөгтийн приметр=",p);
+
+let t = 98;
+let m = t/60;
+let sec = t%60;
+ 
+console.log("4.",Math.floor (m), "мин", sec, "секунд");
+
+let time = 1297;
+let hour = time/3600;
+let min= time/60;
+let secund = time%3600;
+
+console.log("5.",Math.round(hour), "цаг", Math.round (min),"мин", sec, "секунд");
+
+let minut = 875;
+let sekund = 88;
+
+let niilber = minut*60+sekund;
+
+console.log("6. Нийт секунд", niilber);
+
+let tsag = 83
+let minut1 = 84;
+let sekund1 = 85;
+
+let niilber1 = tsag*3600 + minut1*60+sekund1;
+
+console.log("7. Нийт секунд", niilber1);
+
+
+
+
+
 
