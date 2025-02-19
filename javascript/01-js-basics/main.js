589af59e10875a2aacc983ae309c1066cf4f4a09
NaTsO2 <narantsogt.md@gmail.com>
2025-02-19 09:34:54 +0800

    hicheeluud

---
+++ javascript/01-js-basics/main.js
@@ -0,0 +1,20 @@
+console.log("Сайн уу? JS!")
+
+// let message = "Hello, World!";
+// console.log(message);
+
+// let age = 42;
+// console.log(age);
+
+// let gpa=4.5;
+// console.log(gpa);
+
+// let isStudent=true;
+// console.log(isStudent);
+
+// let x;
+// console.log(x);
+
+// let n=null;
+// console.log(n);
+
