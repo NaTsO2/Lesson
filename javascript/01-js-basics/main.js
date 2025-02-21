b0a504c2b6eb6d94fb485c3a305c9a3d0b121799
NaTsO2 <narantsogt.md@gmail.com>
2025-02-21 12:20:03 +0800

    2025.02.21

--- javascript/01-js-basics/main.js
+++ javascript/01-js-basics/main.js
@@ -58,119 +58,414 @@
 // console.log(birthYears [3]);
 // console.log(birthYears [4]);
 // console.log(birthYears [5]);
-// console.log(birthYears [6]);
-// console.log(birthYears [7]);
-// console.log(birthYears [8]);
-// console.log(birthYears [9] = 2024);
+// // console.log(birthYears [6]);
+// // console.log(birthYears [7]);
+// // console.log(birthYears [8]);
+// // console.log(birthYears [9] = 2024);
 
-// console.log(birthYears.length)
+// // console.log(birthYears.length)
 
-// for(let i = 0; i < 100; i++ ) {
-//     console.log(i);
-// }
+// // for(let i = 0; i < 100; i++ ) {
+// //     console.log(i);
+// // }
 
-// let count = 1;
-// while (count <= 10) {
-//     console.log("while loop:", count);
-//     count = count + 1;
-// };
+// // let count = 1;
+// // while (count <= 10) {
+// //     console.log("while loop:", count);
+// //     count = count + 1;
+// // };
 
-// let too = 1;
+// // let too = 1;
 
-// do {
-//     console.log (too);
-//     too = too + 1;   
-// } while(too <= 10);
+// // do {
+// //     console.log (too);
+// //     too = too + 1;   
+// // } while(too <= 10);
 
-// let sum = 0;
-// for (let a = 1; a < 10; a++ ) {
-//     console.log('bodlogo', a);
-//     sum = sum + a;
-// };
-// console.log(sum);
+// // let sum = 0;
+// // for (let a = 1; a < 10; a++ ) {
+// //     console.log('bodlogo', a);
+// //     sum = sum + a;
+// // };
+// // console.log(sum);
 
-// let niilber = 0;
-// let q = 1;
+// // let niilber = 0;
+// // let q = 1;
 
-// while (q < 10) {
-//     niilber = niilber + q;
-//     q++;
-// }
-// console.log("sum =", niilber);
+// // while (q < 10) {
+// //     niilber = niilber + q;
+// //     q++;
+// // }
+// // console.log("sum =", niilber);
 
-// let nas = 17;
+// // let nas = 17;
 
-// if (nas >= 18) {
-//     console.log ("Таны нас:", nas);
-//     console.log("Та насанд хүрсэн байна");
+// // if (nas >= 18) {
+// //     console.log ("Таны нас:", nas);
+// //     console.log("Та насанд хүрсэн байна");
+// // } else {
+// //     console.log("Та насанд хүрээгүй байна");
+// // };
+
+// // let bmi = 24;
+
+// // if (bmi < 18.5) {
+// //     console.log("Та тураалтай байна");
+// // } else if (bmi > 18.5 && bmi < 24.9) {
+// //     console.log("Та хэвийн жинтэй байна");
+// // } else if (bmi >= 25 && bmi <= 29.9) {
+// //     console.log("Та илүүдэл жинтэй байна");
+// // } else if (bmi >= 30 && bmi <=349.9) {
+// //     console.log("Та таргалалттай байна");
+// // } else (bmi < 35 ) 
+// // {
+// //     console.log("Та хэт их таргалалттай байна");
+// // };
+
+// // let a = 3;
+// // let b = 5;
+// // let c = 7;
+// // let p=a+b+c;
+
+// // console.log("1. Гурвалжингийн приметр =",p);
+
+// // let x = [1,3,5,7,8.7];
+// // let sum=x[0]+x[1]+x[2]+x[3];
+
+// // console.log("2. Өгөгдсөн тоонуудын нийлбэр",sum);
+
+
+// // let h = 10;
+// // let w = 5;
+// // let s=h*w;
+// // let pr=(h+w)*2;
+// // console.log("3.тэгш өнцөгтийн талбай=",s,"тэгш өнцөгтийн приметр=",p);
+
+// let t = 98;
+// let m = t/60;
+// let sec = t%60;
+//  
+// console.log("4.",Math.floor (m), "мин", sec, "секунд");
+
+// let time = 20000;
+// let hour = time/3600;
+// let min= time%60;
+// let secund = time%3600;
+
+// console.log("5.",Math.round(hour), "цаг", Math.round (min),"мин", sec, "секунд");
+
+// let minut = 875;
+// let sekund = 88;
+
+// let niilber = minut*60+sekund;
+
+// console.log("6. Нийт секунд", niilber);
+
+// let tsag = 83
+// let minut1 = 84;
+// let sekund1 = 85;
+
+// let niilber1 = tsag*3600 + minut1*60+sekund1;
+
+// console.log("7. Нийт секунд", niilber1);
+
+// // JS Бодлого 2 дах өдөр
+// console.log("Бодлого 1. Өгөгдсөн тооны тэгш сондгойг ол");
+
+// let a = 3;
+// let b = 2;
+// let c = 11;
+// let d = 7;
+
+// let f = a%2;
+// let f1 = b%2;
+// let f2= c%2;
+// let f3 = d%2;
+
+// if (a%2 === 0 ) {
+//     console.log("Бодлого 1. Энэ тоо тэгш байна", a);
 // } else {
-//     console.log("Та насанд хүрээгүй байна");
-// };
+//     console.log("Бодлого 1. Энэ тоо сондгой байна", a);
+// }
 
-// let bmi = 24;
+// if (f1 === 0 ) {
+//     console.log("Бодлого 1. Энэ тоо тэгш байна", b);
+// } else {
+//     console.log("Бодлого 1. Энэ тоо сондгой байна", b);
+// }
+// if (f2 === 0 ) {
+//     console.log("Бодлого 1. Энэ тоо тэгш байна", c);
+// } else {
+//     console.log("Бодлого 1. Энэ тоо сондгой байна", c);
+// }
+// if (f3 === 0 ) {
+//     console.log("Бодлого 1. Энэ тоо тэгш байна", d);
+// } else {
+//     console.log("Бодлого 1. Энэ тоо сондгой байна", d);
+// }
 
-// if (bmi < 18.5) {
-//     console.log("Та тураалтай байна");
-// } else if (bmi > 18.5 && bmi < 24.9) {
-//     console.log("Та хэвийн жинтэй байна");
-// } else if (bmi >= 25 && bmi <= 29.9) {
-//     console.log("Та илүүдэл жинтэй байна");
-// } else if (bmi >= 30 && bmi <=349.9) {
-//     console.log("Та таргалалттай байна");
-// } else (bmi < 35 ) 
-// {
-//     console.log("Та хэт их таргалалттай байна");
-// };
+// console.log("Бодлого 2. Өгөгдсөн 3 тооны багыг ол");
+// let e = 9;
+// let g = 3;
+// let l = 2;
 
-let a = 3;
-let b = 5;
-let c = 7;
-let p=a+b+c;
+// if (e < g && e < l ) {
+//     console.log (e, "тоо хамгийн бага нь");
+// } else if (g < e && g < l) {
+//     console.log(g, "тоо хамгийн бага нь");
+// } else if (l < e && l < g) {
+//     console.log(l, "тоо хамгийн бага нь");
+// } else {console.log("өө чадахгүйн байнөө өөрөө бод");
+// }
 
-console.log("1. Гурвалжингийн приметр =",p);
+// console.log("Бодлого 3. Өгөгдсөн 3 тооны ихийг ол");
+// let too1 = 7;
+// let too2 = 1;
+// let too3 = 12;
 
-let x = [1,3,5,7,8.7];
-let sum=x[0]+x[1]+x[2]+x[3];
+// if (too1 > too2 && too1 > too3 ) {
+//     console.log (too1, "тоо хамгийн их нь");
+// } else if (too2 > too1 && too2 > too3) {
+//     console.log(too2, "тоо хамгийн их нь");
+// } else if (too3 > too1 && too3 > too2) {
+//     console.log(too3, "тоо хамгийн их нь");
+// } else {console.log("өө чадахгүйн байнөө өөрөө бод");
+// }
 
-console.log("2. Өгөгдсөн тоонуудын нийлбэр",sum);
+// console.log("Бодлого 4. Өгөгдсөн 3 тооны 5аас бага тоонуудын үржвэрийг ол");
+
+// let num1 = 9;
+// let num2 = 4;
+// let num3 = 4;
+
+// if (num1 < 5 && num2 < 5) {
+//     m = num1*num2;
+//     console.log("Бодлогын хариу" , m);
+// } else if (num1 < 5 && num3 < 5) {
+//     m = num1*num3;
+//     console.log("Бодлогын хариу" , m);
+// } else if (num2 < 5 && num3 < 5) {
+//     m = num2 * num3;
+//     console.log("Бодлогын хариу" , m);
+// }
+
+// console.log("Бодлого 5. Өгөгдсөн 4 тооны 5аас их тоонуудын үржвэрийг ол");
+
+// let num4 = 11;
+// let num5 = 9;
+// let num6 = 4;
+// let num7 = 3;
+
+// if (num4 > 5 && num5 > 5) {
+//     m1 = num4 * num5;
+//     console.log("Бодлогын хариу" , m1);
+// } else if (num4 > 5 && num6 > 5) {
+//     m1 = num4 * num6;
+//     console.log("Бодлогын хариу" , m1);
+// } else if (num4 > 5 && num7 > 5) {
+//     m1 = num4 * num7;
+//     console.log("Бодлогын хариу" , m1);
+// } else if (num5 > 5 && num6 > 5) {
+//     m1 = num5 * num6;
+//     console.log("Бодлогын хариу" , m1);
+// } else if (num5 > 5 && num7 > 5) {
+//     m1 = num5 * num7;
+//     console.log("Бодлогын хариу" , m1);
+// } else if (num6 > 5 && num7 > 5) {
+//     m1 = num6 * num7;
+//     console.log("Бодлогын хариу" , m1);
+// }
+
+// console.log("Бодлого 6. Өгөгдсөн сар аль улирал вэ?");
+
+// let season = 12;
+
+// if ( season === 1) {
+//     console.log(season, "-р сар өвлийн улирал");
+// } else if (season === 2) {
+//     console.log(season, "-р сар өвлийн улирал");
+// } else if (season === 3) {
+//     console.log(season, "-р сар хаврын улирал");
+// }else if (season === 4) {
+//     console.log(season, "-р сар хаврын улирал");
+// }else if (season === 5) {
+//     console.log(season, "-р сар хаврын улирал");
+// }else if (season === 6) {
+//     console.log(season, "-р сар зуны улирал");
+// }else if (season === 7) {
+//     console.log(season, "-р сар зуны улирал");
+// }else if (season === 8) {
+//     console.log(season, "-р сар зуны улирал");
+// }else if (season === 9) {
+//     console.log(season, "-р сар намрын улирал");
+// }else if (season === 10) {
+//     console.log(season, "-р сар намрын улирал");
+// }else if (season === 11) {
+//     console.log(season, "-р сар намрын улирал");
+// }else if (season === 12) {
+//     console.log(season, "-р сар өвлийн улирал");
+// }
+
+// let sar = 5;
+// if (sar === 12 || sar === 1 || sar === 2) {
+//     console.log(sar, "-р сар өвлийн улирал");
+// } else if (sar === 3 || sar === 4 || sar === 5){
+//     console.log(sar, "-р сар хаврын улирал");
+// } else if (sar === 6 || sar === 7 || sar === 8) {
+//     console.log(sar, "-р сар зуны улирал");
+// } else if (sar === 9 || sar === 10 || sar === 11) {
+//     console.log(sar, "-р сар намрын улирал");
+// }
+
+// console.log("Бодлого 7. Дүн тавь");
+
+// let bat = 67;
+// let ochir = 59;
+
+// if (bat < 60) {
+//     console.log(bat, "Маш муу");
+// } else if ( bat >= 60 && bat <= 70) {
+//     console.log(bat, "хангалттай");
+// } else if ( bat >= 71 && bat <= 80) {
+//     console.log(bat, "Дунд");
+// } else if ( bat >= 80 && bat <= 90) {
+//     console.log(bat, "Сайн");
+// } else if ( bat >= 90 && bat <= 100) {
+//     console.log(bat, "Маш сайн");
+// }
+
+// if (ochir < 60) {
+//     console.log(ochir, "Маш муу");
+// } else if (ochir >= 60 && ochir <= 70) {
+//     console.log(ochir, "хангалттай");
+// } else if (ochir >= 70 && ochir <= 80) {
+//     console.log(ochir, "Дунд");
+// } else if (ochir >= 80 && ochir <= 90) {
+//     console.log(ochir, "Сайн");
+// } else if (ochir >= 90 && ochir <= 100) {
+//     console.log(ochir,"Маш сайн");
+// }
+
+// Bodlogo 3
+console.log("Бодлого 1.================");
+
+let x = 0;
+
+while (x <= 100) {
+    if (x%2 === 0) {
+        console.log( "Эдгээр тоонууд тэгш");
+    }
+    x++;
+}
+
+console.log(x, "Бодлого 2.================");
+
+let y = 0;
+
+while(y <= 100) {
+    if (y%2 === 1) {
+        console.log("Эдгээр тоонууд сондгой");
+    }
+    y++;
+}
+
+console.log("Бодлого 3.================");
+
+let n = 1;
+let m = 1;
+
+while (n <= 200) {
+    if (n%2 === 0) {
+        m = m * n;
+    }
+    n++
+}
+
+console.log("Тэгш тоонуудын үржвэр нь:", m);
+
+console.log("Бодлого 4.================");
+
+let n1 = 1;
+let m1 = 1;
+
+while (n1 <= 200) {
+    if (n1%2 === 1) {
+        m1 = m1 + n1;
+    }
+    n1++
+}
+console.log("Сондгой тоонуудын нийлбэр нь:", m1);
+
+console.log("Бодлого 6.================");
+
+let z = 0, l=0, digit = 0;
+
+while (z <= 148) {
+    digit = z % 10;
+    z = (z - digit)/10;
+    console.log(z);
+}
 
 
-let h = 10;
-let w = 5;
-let s=h*w;
-let pr=(h+w)*2;
-console.log("3.тэгш өнцөгтийн талбай=",s,"тэгш өнцөгтийн приметр=",p);
+console.log("Бодлого 7.==================");
 
-let t = 98;
-let m = t/60;
-let sec = t%60;
- 
-console.log("4.",Math.floor (m), "мин", sec, "секунд");
+let too = 0, total = 0;
 
-let time = 1297;
-let hour = time/3600;
-let min= time/60;
-let secund = time%3600;
+while (too <= 60) {
+    if (too%2 === 0) {
+        total = total + (too%2 == 0)
+        console.log("Тэгш тоо", too);
+    }
+    too++
+}
 
-console.log("5.",Math.round(hour), "цаг", Math.round (min),"мин", sec, "секунд");
+console.log ("Нийт тэгш тоо", total);
 
-let minut = 875;
-let sekund = 88;
+console.log("Бодлого 8.==================");
 
-let niilber = minut*60+sekund;
+let nomer = 6497;
 
-console.log("6. Нийт секунд", niilber);
-
-let tsag = 83
-let minut1 = 84;
-let sekund1 = 85;
-
-let niilber1 = tsag*3600 + minut1*60+sekund1;
-
-console.log("7. Нийт секунд", niilber1);
+    if (nomer%10 === 0 || nomer%10 === 5) {
+        console.log(nomer, "Таны дугаар Баасан гарагт гарахгүй")
+    } else if (nomer%10 === 4 || nomer%10 === 9) {
+        console.log(nomer, "Таны дугаар Пүрэв гарагт гарахгүй")
+    } else if (nomer%10 === 3 ||nomer%10 === 8) {
+        console.log(nomer, "Таны дугаар Лхагва гарагт гарахгүй")
+    } else if (nomer%10 === 2 || nomer%10 === 7) {
+        console.log(nomer, "Таны дугаар Мягмар гарагт гарахгүй")
+    } else if (nomer%10 === 1 || nomer%10 === 6) {
+        console.log(nomer, "Таны дугаар Даваа гарагт гарахгүй")
+    }
 
 
+console.log("Бодлого 9.=================")
 
+let number = 1;
+
+while (number <= 50) {
+    if (number%3 === 0) {
+        console.log(number, "fizz");
+    } else if (number%5 === 0) {
+        console.log(number, "buzz");
+    } else if (number%3 === 0 && number%5 === 0 ) {
+        console.log(number, "fizzbuzz");
+    } else if (number%3 >= 1 || number%5 >= 1) {
+        console.log(number);
+    }
+    number++
+}
+
+const arr = [1, 2, 3, 4, 5];
+
+for (let j=0; j < arr.length; j++) {
+    console.log(arr[j]);
+}
+
+console.log("Бодлого Array 1.===================");
+
+// let birthYear = [1994, 1997, 2003, 1993,2001];
+
+console.log ("hicheel")
 
 
 
